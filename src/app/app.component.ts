a9ba11ae14c26d5222f54a3c31082623caf27332
Bloq Technology <bloqtechnology@gmail.com>
2021-04-05 00:35:42 +0200

    Displaying data and annually and monthly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,20 @@ import { throwError } from 'rxjs/internal/observable/throwError';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit{
   title = 'TaxCalculator-Frontend';
 
   userInput: UserDetails;
-  taxResult: TaxResults;
+  taxResult: TaxResults={netPay:0,payeRaw:0,paye:0,taxCredit:0};
+
+  isAnnual: boolean = true;
+  printPeriod: String;
 
   yearOptions = ['2021','2020'];
   earningTypeOptions = ["Annually", "Monthly"]
 
   taxForm: FormGroup;
-
 
   constructor(private taxService: serverApiService,private builder: FormBuilder) {
 
@@ -30,44 +33,73 @@ export class AppComponent implements OnInit{
       earningType: new FormControl('Annually',Validators.required),
       personAge: new FormControl('25',Validators.required),
       taxYear: new FormControl('2021',Validators.required),
-  });
-
-
-    // this.userInput = {age:42,earningsType:"Annually"
-    //   ,medicalAidDependants:0,taxYear:2021,totalEarnings:498000};
-
+    });
   }
       
 
   ngOnInit(){
-    
+
+    this.printPeriod="Annually";
+    this.send();
   } 
 
   send(){
+    this.buildUserDetails();
 
-    this.setEarningType();
-
-    console.log(this.userInput);
-    this.taxService.getTax(this.userInput).subscribe(userTaxResults=>{
-      this.taxResult=userTaxResults;
-      console.log(userTaxResults);
+    this.taxService.getTax(this.userInput).subscribe((userTaxResults:TaxResults)=>{
+      //this.taxResult=userTaxResults;
+      this.taxResult = {
+        paye: Number((userTaxResults.paye).toFixed(2)),
+        payeRaw: Number((userTaxResults.payeRaw).toFixed(2)),
+        netPay: Number((userTaxResults.netPay).toFixed(2)),
+        taxCredit: Number((userTaxResults.taxCredit).toFixed(2))
+      }
+      
+      this.printPeriod="Annually";
     });
   }
 
-  setEarningType(){
+  buildUserDetails(){
 
-    if(this.userInput.earningsType=="Monthly"){
-
-      this.userInput.earningsType=EarningType.MONTHLY;
-
-    }
-    else if(this.userInput.earningsType=="Annually"){
-      this.userInput.earningsType=EarningType.ANNUALLY;
-    }
-    else{
-      return throwError("Invalid Period");
-    }
-
-  }
+    let earnigsEnum = 
+    this.taxForm.value.earningType=="Monthly"?EarningType.MONTHLY:EarningType.ANNUALLY;
     
+    this.userInput = {
+      age:this.taxForm.value.personAge,
+      totalEarnings: this.taxForm.value.grossSalary,
+      earningsType: earnigsEnum,
+      medicalAidDependants: 0,
+      taxYear: this.taxForm.value.taxYear}
+  }
+
+  
+  togglePrintPeriod(){
+
+    if(this.printPeriod=="Annually"){
+  
+      //This is solely for display on the results table
+      this.printPeriod = "Monthly";
+      
+      //Fix to 2 decimal places
+      //Should have opted to use currency api
+      this.taxResult = {
+        paye: Number((this.taxResult.paye/12).toFixed(2)),
+        payeRaw: Number((this.taxResult.payeRaw/12).toFixed(2)),
+        netPay: Number((this.taxResult.netPay/12).toFixed(2)),
+        taxCredit: Number((this.taxResult.taxCredit/12).toFixed(2))}
+    }
+    else if(this.printPeriod=="Monthly"){
+
+      this.printPeriod = "Annually";
+
+      //Fix to 2 decimal places
+      //Should have opted to use currency api
+      this.taxResult = {
+        paye: Number((this.taxResult.paye*12).toFixed(2)),
+        payeRaw: Number((this.taxResult.payeRaw*12).toFixed(2)),
+        netPay: Number((this.taxResult.netPay*12).toFixed(2)),
+        taxCredit: Number((this.taxResult.taxCredit*12).toFixed(2))
+      }
+    }
+  }
 }
